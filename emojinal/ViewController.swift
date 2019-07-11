06be3911d8389fd1b1076a7473776d45d2ff438c
Apple <apple@Apples-MacBook-Pro-106.local>
2019-07-11 15:48:28 -0700

    emoji app

---
+++ emojinal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  emojinal
+//
+//  Created by Apple on 7/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func happyButton(_ sender: Any) {
+        _ = (title : "I am so glad you're happy!", message : "I recommend listening to Formation and Who Run The World")
+    }
+    @IBAction func loveBotton(_ sender: Any) {
+    }
+    @IBAction func sadButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
