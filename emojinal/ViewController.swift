fd0ea504cbd90e19a93ffc444e1eaa369f323a66
Apple <apple@Apples-MacBook-Pro-106.local>
2019-07-11 16:19:12 -0700

    updated emojinal

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func happyButton(_ sender: Any) {
-        _ = (title : "I am so glad you're happy!", message : "I recommend listening to Formation and Who Run The World")
+    @IBAction func happyButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "I'm so glad you're happy!", message: "Try listening to the songs Formation and Who Run The World.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+        self.present(alert, animated: true)
     }
-    @IBAction func loveBotton(_ sender: Any) {
+    @IBAction func loveBotton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "It's great to be in love!", message: "Try listening to the songs Drunk In Love and Crazy In Love.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+        self.present(alert, animated: true)
     }
-    @IBAction func sadButton(_ sender: Any) {
+    @IBAction func sadButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "I'm sorry you're sad!", message: "Try listening to the songs Pretty Hurts and Sandcastles.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+        self.present(alert, animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
