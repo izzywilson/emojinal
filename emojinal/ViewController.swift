7830777e13e6264fab302598c0732be7113f992d
Apple <apple@Apples-MacBook-Pro-106.local>
2019-07-12 10:28:19 -0700

    iteration 3

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,21 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let emojis = ["😀": "I'm so glad you're happy!", "😍": "It's great to be in love!", "😰": "I'm sorry you're sad!"]
+    var customMessages = ["I'm so glad you're happy!": ["Try listening Formation","Try listening to Who Run The World", "Try listening to Halo", "Try listening to Single Ladies"], "It's great to be in love!": ["Try listening to Drunk In love", "Try listening to Crazy in Love", "Try listening to Love On Top", "Try listening to Say my name"],"I'm sorry you're sad!": ["Try listening to Pretty Hurts", "Try listening to Sandcastles", "try listening to If I Were A Boy", "Try listening to Pray You Catch Me"]]
 
     @IBAction func happyButton(_ sender: UIButton) {
-        let alert = UIAlertController(title: "I'm so glad you're happy!", message: "Try listening to the songs Formation and Who Run The World.", preferredStyle: .alert)
+        let selectedEmotion = sender.titleLabel?.text
+         customMessages[emojis[selectedEmotion!]!]!.shuffle()
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[0]
+        let alert = UIAlertController(title:emojis["😀"]!, message: emojiMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
         self.present(alert, animated: true)
+        alert.addAction(UIAlertAction(title: "Bye", style: .default, handler: nil))
     }
     @IBAction func loveBotton(_ sender: UIButton) {
-        let alert = UIAlertController(title: "It's great to be in love!", message: "Try listening to the songs Drunk In Love and Crazy In Love.", preferredStyle: .alert)
+     let selectedEmotion = sender.titleLabel?.text
+        customMessages[emojis[selectedEmotion!]!]!.shuffle()
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[0]
+        let alert = UIAlertController(title:emojis["😍"]!, message: emojiMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
         self.present(alert, animated: true)
+        alert.addAction(UIAlertAction(title: "Bye", style: .default, handler: nil))
     }
     @IBAction func sadButton(_ sender: UIButton) {
-        let alert = UIAlertController(title: "I'm sorry you're sad!", message: "Try listening to the songs Pretty Hurts and Sandcastles.", preferredStyle: .alert)
+        let selectedEmoji = sender.titleLabel?.text
+        customMessages[emojis[selectedEmoji!]!]!.shuffle()
+        let emojiMessage = customMessages[emojis[selectedEmoji!]!]?[0]
+        let alert = UIAlertController(title:emojis["😰"]!, message: emojiMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
         self.present(alert, animated: true)
+         alert.addAction(UIAlertAction(title: "Bye", style: .default, handler: nil))
     }
     override func viewDidLoad() {
         super.viewDidLoad()
